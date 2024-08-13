ba7a67aecb4d5b69a544f1d0c61a63873936457e
AMAN KOTHARI <167640821+aman27052002@users.noreply.github.com>
2024-08-13 13:44:45 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
-
-import { useState } from "react";
-// import './app.css' 
 import Form from './components/Form/Form'
+import { useState } from 'react';
 export default function App() {
-  const [btn,setBtn] = useState(false)
+  const [showForm,setshowForm] = useState(false);
   return (
-    <div className="flex items-center justify-center min-h-screen p-4  bg-blue-500">
-      <Form />
+    <div className="flex items-center flex-col gap-6 h-screen p-4 bg-[#14161b]">
+      
+      <button onClick={()=>setshowForm(true)} className='bg-indigo-500 font-bold px-4 py-4 rounded-lg text-lg '>Add Book</button>
+      {showForm && <Form onClose={()=>setshowForm(false)} />}
     </div>
   );
 }
